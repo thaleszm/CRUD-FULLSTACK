dea1b7d890165ee824f19646bc7d729369d47c6c
thaleszm <marquesthales345@gmail.com>
2026-05-20 13:10:12 -0300

    debug: log railway env variables

--- crud-backend/src/db.js
+++ crud-backend/src/db.js
@@ -19,12 +19,10 @@ const pool = mysql.createPool({
   }
 });
 
-console.log({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  db: process.env.DB_NAME
-});
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME:", process.env.DB_NAME);
 console.log("MySQL conectado");
 
 export const query = async (sql, params = []) => {
