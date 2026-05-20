648c357101d64734b4eb8d1975b053703181433b
thaleszm <marquesthales345@gmail.com>
2026-05-20 09:19:03 -0300

    add: add new db.js

--- crud-backend/src/db.js
+++ crud-backend/src/db.js
@@ -3,18 +3,26 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const connection = await mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 console.log("MySQL conectado");
 
 export const query = async (sql, params = []) => {
-  const [rows] = await connection.execute(sql, params);
+  const [rows] = await pool.execute(sql, params);
 
   return {
     rows,
