822002aae42dfdf422f7c3728e518ebbeff0c822
thaleszm <marquesthales345@gmail.com>
2026-05-20 16:37:39 -0300

    refactor: replace MySQL with PostgreSQL (pg) and update database connection

--- crud-backend/src/db.js
+++ crud-backend/src/db.js
@@ -1,37 +1,15 @@
-import mysql from "mysql2/promise";
+import pg from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+const { Pool } = pg;
 
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false
-  }
+    rejectUnauthorized: false,
+  },
 });
 
-console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_PORT:", process.env.DB_PORT);
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_NAME:", process.env.DB_NAME);
-console.log("MySQL conectado");
-
-export const query = async (sql, params = []) => {
-  const [rows] = await pool.execute(sql, params);
-
-  return {
-    rows,
-    rowCount: Array.isArray(rows)
-      ? rows.length
-      : rows.affectedRows
-  };
-};
+console.log("🔥 PostgreSQL conectado");
